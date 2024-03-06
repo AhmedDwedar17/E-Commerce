41fbbaf24293c9eaa2baa8a208a990f30d373c16
Ahmed Dwedar <140907573+AhmedDwedar17@users.noreply.github.com>
2024-03-06 21:24:40 +0200

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "@fortawesome/fontawesome-free/css/all.min.css"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.bundle.min.js"
+import './index.css';
+import TokenContextProvider from './Context/TokenContext';
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import CartProvider from './Context/Cart';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const queryClient = new QueryClient()
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <CartProvider>
+  <QueryClientProvider client={queryClient}>
+  <TokenContextProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </TokenContextProvider>
+  </QueryClientProvider>
+  </CartProvider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
